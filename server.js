c3399ad12cdc66bf5386f8545dceb2fc19004d0f
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-10-20 17:32:22 +0530

    Backend Completed

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const ruleRoutes = require('./routes/ruleRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 2000;
+
+app.use(cors());
+app.use(bodyParser.json());
+// app.use(cors()); 
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+mongoose.connect('mongodb://127.0.0.1:27017/SampleCheck', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log("Connected to MongoDB");
+}).catch(err => {
+  console.error("Error connecting to MongoDB", err);
+});
+
+// API routes
+app.use('/api/rules', ruleRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
